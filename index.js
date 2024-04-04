c6918a2130cc9d14c7427665b6dd1c1818b48ccf
MRMutahir <mutahirkareem820@gmail.com>
2024-04-04 15:17:59 +0500

    done

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import cookieParser from "cookie-parser";
 import { membersRoutes } from "./View/Members.js";
 import { booksRoutes } from "./View/Books.js";
 import { BorrowingRoutes } from "./View/Borrowing.js";
-import { errorHandler } from "./Error.js";
+
 import { reservationRoutes } from "./View/reservation.js";
 
 dotenv.config();
@@ -17,7 +17,6 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-app.use(errorHandler);
 
 const connect = async () => {
   try {
